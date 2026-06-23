d38ca0c2287d7dd6fe762235ac9dded59337a5bb
Diop Ousmane Sarr <ousmane-sarr.diop@etu.univ-orleans.fr>
2026-06-23 11:33:51 +0200

    ajout des models : campaign et user

---
+++ app/models/campaign.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, Integer, String, Enum, Date, Float 
+from sqlalchemy.orm import relationship
+from app.database import Base
+from datetime import datetime, date
+from enum import Enum
+from sqlalchemy import ForeignKey
+
+class CampaignStatus(Enum):
+    EN_ATTENTE = "en_attente"
+    ACTIVE = "active"
+    FINANCEE = "financee"
+    CLOTUREE = "cloturee"
+    SUSPENDUE = "suspendue"
+
+
+class Campaign(Base):
+    __tablename__ = "campaigns"
+
+    id = Column(Integer, primary_key=True, index=True)
+    titre = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    secteur = Column(String, nullable=False)
+    objectifFinancier = Column(Integer, nullable=False)
+    dateDebut = Column(Date, nullable=False)
+    dateFin = Column(Date, nullable=False)
+    statut = Column(Enum(CampaignStatus), default=CampaignStatus.EN_ATTENTE, nullable=False)
+    prixUnitairePart = Column(Integer, nullable=False)
+    nombreTotalParts = Column(Integer, nullable=False)
+    tauxRendement = Column(Float, nullable=False)
+    dureRemboursement = Column(Integer, nullable=False)
+    owner_id = Column(Integer, ForeignKey("project_owners.id"), nullable=False)
+
+    owner = relationship("User", back_populates="campaigns")    
+
+
+
---
+++ app/models/user.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from app.database import Base
+from datetime import datetime
+from enum import Enum
+
+class UserStatus(Enum):
+    EN_ATTENTE ="en_attente"
+    VALIDE = "valide"
+    SUSPENDU = "suspendu"
+
+class Role(Enum):
+    INVESTOR = "investor"
+    PROJECT_OWNER = "project_owner"
+    ADMIN = "admin"
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    status = Column(Enum(UserStatus), default=UserStatus.EN_ATTENTE, nullable=False)
+    secteur_activite = Column(String, nullable=True) 
+    role = Column(String, nullable=False) 
+
+    campaigns = relationship("Campaign", back_populates="owner")
+
+    
